425053cf756cd350861d0a521275e7859d0e9e24
mac <mac@macdeMacBook-Pro.local>
2017-12-16 23:35:50 +0800

    update

--- src/containers/GeMin/components/Panel/Panel.js
+++ src/containers/GeMin/components/Panel/Panel.js
@@ -9,10 +9,8 @@ export default class Panel extends Component {
     render() {
         return (
           <div id="Panelpage">
-            <div className="main-panel ps-container ps-theme-default ps-active-y" data-ps-id="557d8ba7-6480-8b7c-f7ae-3de6bb1e5179">
-              <Panelheader />
-              <Panelcontent />
-            </div>
+            <Panelheader />
+            <Panelcontent />
           </div>
         );
     }
--- src/containers/GeMin/components/Panelheader/Panelheader.js
+++ src/containers/GeMin/components/Panelheader/Panelheader.js
@@ -1,55 +1,38 @@
 import React, { Component } from "react";
 import FontAwesome from 'react-fontawesome';
 import { Button } from 'reactstrap';
+import './Panelheader.less';
 
 export default class Panelheader extends Component {
-    render() {
-        return (
-          <nav className="navbar navbar-transparent navbar-absolute">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#"> Material Dashboard </a>
-              </div>
-              <div >
-                <ul className="nav navbar-nav navbar-right">
-                  <li className="th">
-                    <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#pablo" className="dropdown-toggle" data-toggle="dropdown">
-                      <div>
-                        <FontAwesome name="th-large" />
-                      </div>
-                    </a>
-                  </li>
-                  <li className="bell">
-                    <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#" className="dropdown-toggle" data-toggle="dropdown">
-                      <span className="notification">5</span>
-                      <div>
-                        <FontAwesome name="bell" />
-                      </div>
-                    </a>
-                  </li>
-                  <li className="user">
-                    <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#pablo" className="dropdown-toggle" data-toggle="dropdown">
-                      <div>
-                        <FontAwesome name="user" />
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-                <form className="navbar-form navbar-right" role="search">
-                  <div className="form-group is-empty">
-                    <input type="text" className="form-control" placeholder="Search" />
-                    <span className="material-input" />
-                    <span className="material-input" />
-                  </div>
-                  <Button color="secondary" type="submit" className="btn">
-                    <div>
-                      <FontAwesome name="search" />
-                    </div>
-                  </Button>
-                </form>
-              </div>
+  render() {
+    return (
+      <div id="Panelheader">
+        <div className="Panelheader-header">
+          <a className="navbar-brand" href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#"> Material Dashboard </a>
+        </div>
+        <ul className="Panelheader-icon">
+          <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#pablo" className="th">
+            <FontAwesome name="th-large" />
+          </a>
+          <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#" className="bell">
+            <span className="notification">5</span>
+            <FontAwesome name="bell" />
+          </a>
+          <a href="http://demos.creative-tim.com/material-dashboard/examples/dashboard.html?_ga=2.28571388.179549990.1512119107-699052212.1512119107#pablo" className="user">
+            <FontAwesome name="user" />
+          </a>
+        </ul>
+        <form className="Panelheader-search" role="search">
+          <div className="Panelheader-input">
+            <input type="text" placeholder="Search" />
+          </div>
+          <Button color="secondary" type="submit" className="btn">
+            <div>
+              <FontAwesome name="search" />
             </div>
-          </nav>
-        );
-    }
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
